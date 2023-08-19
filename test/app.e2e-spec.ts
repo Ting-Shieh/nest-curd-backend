74443943d340dc7fa388ca37f97f082a93b3e2aa
Ting-Shieh <zsting29@gmail.com>
2023-08-19 16:02:47 +0800

    feat: pactum 安裝集成->全局掛載

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,16 @@
+import * as pactum from 'pactum';
+
 describe('AppController (e2e)', () => {
+  let spec;
+  beforeEach(() => {
+    // console.log('app', global.app);
+    pactum.request.setBaseUrl('http://localhost:3000');
+    // spec = global.pactum as Spec;
+  });
   it('/ (GET)', () => {
-    // return request(app.getHttpServer())
-    //   .get('/api/v1')
-    //   .expect(200)
-    //   .expect('Hello World!');
+    return pactum
+      .spec('/api/v1/auth')
+      .expectStatus(200)
+      .expectBodyContains('Hello World!');
   });
 });
--- test/setup-jest.ts
+++ test/setup-jest.ts
@@ -6,10 +6,15 @@ let appFactory: AppFactory;
 let app: INestApplication;
 global.beforeEach(async () => {
   appFactory = await AppFactory.init();
+  // 防止端口被佔用
+  await appFactory?.destory();
   await appFactory.initDB();
   app = appFactory.instance;
+  global.app = app; // 全局掛載
 });
 
 global.afterEach(async () => {
-  await appFactory.cleanup();
+  await appFactory?.cleanup();
+  // 關閉進程
+  await app?.close();
 });
